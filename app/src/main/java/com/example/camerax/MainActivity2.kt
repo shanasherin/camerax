3f9ec51109937066ab183e457be732e95e5ee43f
shana sherin <shanarinu1@gmail.com>
2019-12-25 09:53:56 +0400

    first commit

---
+++ app/src/main/java/com/example/camerax/MainActivity2.kt
@@ -0,0 +1,30 @@
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Matrix
+import android.os.Bundle
+import android.util.Size
+import android.view.Surface
+import android.view.TextureView
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.camerax.R
+import java.util.concurrent.Executors
+
+// Your IDE likely can auto-import these classes, but there are several
+// different implementations so we list them here to disambiguate.
+
+class MainActivity2 : AppCompatActivity() {
+
+
+
+}
+
+
+
